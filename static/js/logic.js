6b4fdc5e303c6345ad32833b40b81dac950e03fd
arunbalaram89 <53626186+arunbalaram89@users.noreply.github.com>
2023-10-08 19:09:32 -0400

    Add files via upload
    Files used to create visualizations + Leaflet map.

---
+++ static/js/logic.js
@@ -0,0 +1,119 @@
+const earthquake_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+
+
+
+d3.json(earthquake_url).then(function(data) {
+    console.log(data);
+    createFeatures(data.features);
+  });
+
+
+  function createFeatures(earthquakeData) {
+
+    // Define a function that we want to run once for each feature in the features array.
+    // Give each feature a popup that describes the place and time of the earthquake.
+    function onEachFeature(feature, layer) {
+      layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
+    }
+    function pointToLayer(geoJsonPoint, latlng) {
+        return L.circleMarker(latlng);
+    }
+    
+    function style(feature) {
+        console.log(feature)
+        return {
+            color: "green",
+            // use get color function (find which feature.properties to use)
+            fillColor: "#84f004",
+            fillOcapacity: feature.geometry.coordinates[2] * 1.5,
+            weight: 1,
+            radius: feature.properties.mag * 5
+        }
+
+    }
+    
+    // Create a GeoJSON layer that contains the features array on the earthquakeData object.
+    // Run the onEachFeature function once for each piece of data in the array.
+    let earthquakes = L.geoJSON(earthquakeData, {
+      onEachFeature: onEachFeature,
+      pointToLayer: pointToLayer,
+      style: style
+    });
+  
+    // Send our earthquakes layer to the createMap function/
+    createMap(earthquakes);
+  }
+  
+  function createMap(earthquakes) {
+  
+    // Create the base layers.
+    let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    })
+  
+    let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+      attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+    });
+  
+    // Create a baseMaps object.
+    let baseMaps = {
+      "Street Map": street,
+      "Topographic Map": topo
+    };
+  
+    // Create an overlay object to hold our overlay.
+    let overlayMaps = {
+      Earthquakes: earthquakes
+    };
+  
+    // Create our map, giving it the streetmap and earthquakes layers to display on load.
+    let myMap = L.map("map", {
+      center: [
+        37.09, -95.71
+      ],
+      zoom: 5,
+      layers: [street, earthquakes]
+    });
+  
+    // Create a layer control.
+    // Pass it our baseMaps and overlayMaps.
+    // Add the layer control to the map.
+    L.control.layers(baseMaps, overlayMaps, {
+      collapsed: false
+    }).addTo(myMap);
+  
+    var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+    // Grades will have to be adjusted 
+        grades = [-10, 10, 30, 50, 70, 90],
+        labels = [];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+};
+
+legend.addTo(myMap);
+
+  }
+
+// LEGEND (colors to be adjusted)
+
+function getColor(d) {
+    return d > 1000 ? '#800026' :
+           d > 500  ? '#BD0026' :
+           d > 200  ? '#E31A1C' :
+           d > 100  ? '#FC4E2A' :
+           d > 50   ? '#FD8D3C' :
+           d > 20   ? '#FEB24C' :
+           d > 10   ? '#FED976' :
+                      '#FFEDA0';
+}
